fb61758d05aae177065dc03475e8811d02de8582
klaus <haibara.yyh@gmail.com>
2023-09-24 05:52:53 -0400

    3.4.11

--- PaymentControl/UPPaymentControl.h
+++ PaymentControl/UPPaymentControl.h
@@ -4,7 +4,7 @@
 //
 //  Created by qcao on 15/10/20.
 //  Copyright © 2015年 China Unionpay Co.,Ltd. All rights reserved.
-//  v3.4.10 build1(mini)
+//  v3.4.11 build0(mini)
 //
 
 #import <Foundation/Foundation.h>
